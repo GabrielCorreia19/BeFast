4e7560aafc29a66b9512469910c855eeebe9a9b4
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2025-03-13 12:41:19 -0300

    feat: Criando tela de Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-login',
   imports: [HeaderComponent, MatIconModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
-}
+export class LoginComponent {}
